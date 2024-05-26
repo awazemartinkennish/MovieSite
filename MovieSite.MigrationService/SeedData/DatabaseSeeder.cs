d10aae1e7312cf11569cf9d6a2321d241239bc8c
Martin Kennish <billthemaxster@gmail.com>
2024-05-26 21:17:04 +0100

    fix - database seeder script

--- MovieSite.MigrationService/SeedData/DatabaseSeeder.cs
+++ MovieSite.MigrationService/SeedData/DatabaseSeeder.cs
@@ -17,7 +17,7 @@ namespace MovieSite.MigrationService.SeedData
 
         public (List<Movie> movies, List<Screen> screens, List<MovieScreening> screenings, List<Ticket> tickets) GenerateData()
         {
-            int movieIds = 0;
+            int movieIds = 1;
 
             var movies = new Faker<Movie>()
                 .RuleFor(m => m.Id, f => movieIds++)
@@ -30,19 +30,19 @@ namespace MovieSite.MigrationService.SeedData
                 .RuleFor(m => m.BoardRating, f => f.PickRandom<Movie.Rating>())
                 .Generate(10);
 
-            int screenIds = 0;
+            int screenIds = 1;
             var screens = new Faker<Screen>()
                 .RuleFor(s => s.Id, f => screenIds++)
-                .RuleFor(s => s.Name, f => $"Screen {screenIds}")
+                .RuleFor(s => s.Name, f => $"Screen {screenIds - 1}")
                 .RuleFor(s => s.Capacity, f => f.Random.Number(50, 500))
                 .Generate(10);
 
-            int screeningIds = 0;
-            DateTimeOffset screeningTime = DateTimeOffset.UtcNow;
+            int screeningIds = 1;
+            DateTimeOffset screeningTime = DateTimeOffset.UtcNow.AddHours(8).ToOffset(TimeSpan.Zero);
             int screeningScreenId = 1;
             var screenings = new Faker<MovieScreening>()
                 .RuleFor(s => s.Id, f => screeningIds++)
-                .RuleFor(s => s.MovieId, f => f.Random.Number(0, movies.Count - 1))
+                .RuleFor(s => s.MovieId, f => f.Random.Number(1, movies.Count))
                 .RuleFor(s => s.ScreenId, f =>
                 {
                     if (screeningScreenId == screens.Count)
@@ -69,13 +69,13 @@ namespace MovieSite.MigrationService.SeedData
 
                     return screeningTime;
                 })
-                .RuleFor(s => s.TicketPrice, f => f.Random.Decimal(5, 20))
+                .RuleFor(s => s.TicketPrice, f => Math.Round(f.Random.Decimal(5, 20), 2))
                 .Generate(50);
 
-            int ticketIds = 0;
+            int ticketIds = 1;
             var tickets = new Faker<Ticket>()
                 .RuleFor(t => t.Id, f => ticketIds++)
-                .RuleFor(t => t.MovieScreeningId, f => f.Random.Number(0, screenings.Count - 1))
+                .RuleFor(t => t.MovieScreeningId, f => f.Random.Number(1, screenings.Count))
                 .RuleFor(t => t.SeatNumber, f => f.Random.Number(1, 50))
                 .Generate(100);
 
--- MovieSite.MigrationService/Worker.cs
+++ MovieSite.MigrationService/Worker.cs
@@ -7,14 +7,15 @@ using Microsoft.EntityFrameworkCore.Storage;
 using OpenTelemetry.Trace;
 
 using MovieSite.Database;
-using MovieSite.Database.Models;
 using MovieSite.MigrationService.SeedData;
+using System.Text.Json;
 
 namespace MovieSite.MigrationService;
 
 public class Worker(
     IServiceProvider serviceProvider,
-    IHostApplicationLifetime hostApplicationLifetime) : BackgroundService
+    IHostApplicationLifetime hostApplicationLifetime,
+    ILogger<Worker> logger) : BackgroundService
 {
     public const string ActivitySourceName = "Migrations";
     private static readonly ActivitySource s_activitySource = new(ActivitySourceName);
@@ -41,7 +42,7 @@ public class Worker(
         hostApplicationLifetime.StopApplication();
     }
 
-    private static async Task EnsureDatabaseAsync(MovieDbContext dbContext, CancellationToken cancellationToken)
+    private async Task EnsureDatabaseAsync(MovieDbContext dbContext, CancellationToken cancellationToken)
     {
         var dbCreator = dbContext.GetService<IRelationalDatabaseCreator>();
 
@@ -57,7 +58,7 @@ public class Worker(
         });
     }
 
-    private static async Task RunMigrationAsync(MovieDbContext dbContext, CancellationToken cancellationToken)
+    private async Task RunMigrationAsync(MovieDbContext dbContext, CancellationToken cancellationToken)
     {
         var strategy = dbContext.Database.CreateExecutionStrategy();
         await strategy.ExecuteAsync(async () =>
@@ -69,10 +70,11 @@ public class Worker(
         });
     }
 
-    private static async Task SeedDataAsync(MovieDbContext dbContext, CancellationToken cancellationToken)
+    private async Task SeedDataAsync(MovieDbContext dbContext, CancellationToken cancellationToken)
     {
         DatabaseSeeder seeder = new();
         var (movies, screens, screenings, tickets) = seeder.GenerateData();
+        logger.LogInformation($"Data generated { JsonSerializer.Serialize(new { Movies = movies, Screens = screens, Screenings = screenings, Tickets = tickets })}");
 
         var strategy = dbContext.Database.CreateExecutionStrategy();
         await strategy.ExecuteAsync(async () =>
@@ -82,7 +84,7 @@ public class Worker(
             await dbContext.Movies.AddRangeAsync(movies, cancellationToken);
             await dbContext.Screens.AddRangeAsync(screens, cancellationToken);
             await dbContext.MovieScreenings.AddRangeAsync(screenings, cancellationToken);
-            await dbContext.Tickets.AddRangeAsync(tickets, cancellationToken);  
+            await dbContext.Tickets.AddRangeAsync(tickets, cancellationToken);
             await dbContext.SaveChangesAsync(cancellationToken);
             await transaction.CommitAsync(cancellationToken);
         });
