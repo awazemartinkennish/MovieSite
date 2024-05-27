3dad61858c620472deadad6a60259e8fe0196781
Martin Kennish <billthemaxster@gmail.com>
2024-05-27 08:59:14 +0100

    fix - db seeder to use item refences
    So it'll autogenerate IDs

--- MovieSite.MigrationService/SeedData/DatabaseSeeder.cs
+++ MovieSite.MigrationService/SeedData/DatabaseSeeder.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Bogus;
+﻿using Bogus;
 using MovieSite.Database.Models;
 
 namespace MovieSite.MigrationService.SeedData
@@ -15,14 +10,114 @@ namespace MovieSite.MigrationService.SeedData
             Randomizer.Seed = new Random(20240526);
         }
 
+        private readonly List<string> movieTitles = new()
+        {
+            "The Godfather",
+            "The Shawshank Redemption",
+            "Schindler's List",
+            "Raging Bull",
+            "Casablanca",
+            "Citizen Kane",
+            "Gone with the Wind",
+            "The Wizard of Oz",
+            "Gone With the Wind",
+            "Lawrence of Arabia",
+            "Vertigo",
+            "Psycho",
+            "Pulp Fiction",
+            "Fight Club",
+            "The Dark Knight",
+            "Inception",
+            "The Matrix",
+            "Forrest Gump",
+            "Goodfellas",
+            "The Silence of the Lambs",
+            "The Lord of the Rings: The Fellowship of the Ring",
+            "The Lord of the Rings: The Two Towers",
+            "The Lord of the Rings: The Return of the King",
+            "The Avengers",
+            "Interstellar",
+            "The Departed",
+            "Gladiator",
+            "The Lion King",
+            "Titanic",
+            "Avatar",
+            "The Green Mile",
+            "The Prestige",
+            "The Social Network",
+            "The Grand Budapest Hotel",
+            "Whiplash",
+            "La La Land",
+            "The Revenant",
+            "Birdman",
+            "The Shape of Water",
+            "Moonlight",
+            "Get Out",
+            "Black Panther",
+            "The Big Lebowski",
+            "Eternal Sunshine of the Spotless Mind",
+            "No Country for Old Men",
+            "The Departed",
+            "The Wolf of Wall Street",
+            "Her",
+            "Drive",
+            "Mad Max: Fury Road",
+            "The Martian",
+            "Arrival",
+            "Blade Runner 2049",
+            "Baby Driver",
+            "Dunkirk",
+            "Three Billboards Outside Ebbing, Missouri",
+            "Call Me by Your Name",
+            "Lady Bird",
+            "A Star Is Born",
+            "Bohemian Rhapsody",
+            "Joker",
+            "Parasite",
+            "1917",
+            "Once Upon a Time in Hollywood",
+            "Knives Out",
+            "Tenet",
+            "Soul",
+            "Nomadland",
+            "Minari",
+            "Promising Young Woman",
+            "Sound of Metal",
+            "The Trial of the Chicago 7",
+            "Mank",
+            "Judas and the Black Messiah",
+            "The Father",
+            "Ma Rainey's Black Bottom",
+            "One Night in Miami",
+            "The Irishman",
+            "Marriage Story",
+            "Jojo Rabbit",
+            "Little Women",
+            "Ford v Ferrari",
+            "1917",
+            "The Farewell",
+            "Uncut Gems",
+            "The Lighthouse",
+            "Portrait of a Lady on Fire",
+            "Parasite",
+            "Pain and Glory",
+            "Marriage Story",
+            "Joker",
+            "Jojo Rabbit",
+            "Ford v Ferrari",
+            "1917",
+            "Once Upon a Time in Hollywood",
+            "Little Women",
+            "The Irishman",
+            "Parasite"
+        };
+
         public (List<Movie> movies, List<Screen> screens, List<MovieScreening> screenings, List<Ticket> tickets) GenerateData()
         {
-            int movieIds = 1;
-
             var movies = new Faker<Movie>()
-                .RuleFor(m => m.Id, f => movieIds++)
-                .RuleFor(m => m.Title, f => f.Random.Words(5))
-                .RuleFor(m => m.Genre, f => f.PickRandom("Action", "Comedy", "Drama", "Horror", "Sci-Fi"))
+                .RuleFor(m => m.Id, f => 0)
+                .RuleFor(m => m.Title, f => f.PickRandom(movieTitles))
+                .RuleFor(m => m.Genre, f => f.PickRandom("Action", "Comedy", "Drama", "Horror", "Sci-Fi", "Adventure"))
                 .RuleFor(m => m.ReleaseDate, f => f.Date.BetweenDateOnly(
                     DateOnly.FromDateTime(DateTime.Now.AddMonths(-3)),
                     DateOnly.FromDateTime(DateTime.Now)))
@@ -30,30 +125,30 @@ namespace MovieSite.MigrationService.SeedData
                 .RuleFor(m => m.BoardRating, f => f.PickRandom<Movie.Rating>())
                 .Generate(10);
 
-            int screenIds = 1;
+            var screenIndex = 1;
             var screens = new Faker<Screen>()
-                .RuleFor(s => s.Id, f => screenIds++)
-                .RuleFor(s => s.Name, f => $"Screen {screenIds - 1}")
+                .RuleFor(s => s.Id, f => 0)
+                .RuleFor(s => s.Name, f => $"Screen {screenIndex++}")
                 .RuleFor(s => s.Capacity, f => f.Random.Number(50, 500))
                 .Generate(10);
 
-            int screeningIds = 1;
+            int screeningScreenIndex = 0;
             DateTimeOffset screeningTime = DateTimeOffset.UtcNow.AddHours(8).ToOffset(TimeSpan.Zero);
-            int screeningScreenId = 1;
             var screenings = new Faker<MovieScreening>()
-                .RuleFor(s => s.Id, f => screeningIds++)
-                .RuleFor(s => s.MovieId, f => f.Random.Number(1, movies.Count))
-                .RuleFor(s => s.ScreenId, f =>
+                .RuleFor(s => s.Id, f => 0)
+                .RuleFor(s => s.Movie, f => f.PickRandom(movies))
+                .RuleFor(s => s.Screen, f =>
                 {
-                    if (screeningScreenId == screens.Count)
+                    if (screeningScreenIndex == screens.Count - 1)
                     {
-                        screeningScreenId = 1;
+                        screeningScreenIndex = 0;
                     }
                     else
                     {
-                        screeningScreenId++;
+                        screeningScreenIndex++;
                     }
-                    return screeningScreenId;
+
+                    return screens[screeningScreenIndex];
                 })
                 .RuleFor(s => s.ScreeningTime, f =>
                 {
@@ -72,10 +167,9 @@ namespace MovieSite.MigrationService.SeedData
                 .RuleFor(s => s.TicketPrice, f => Math.Round(f.Random.Decimal(5, 20), 2))
                 .Generate(50);
 
-            int ticketIds = 1;
             var tickets = new Faker<Ticket>()
-                .RuleFor(t => t.Id, f => ticketIds++)
-                .RuleFor(t => t.MovieScreeningId, f => f.Random.Number(1, screenings.Count))
+                .RuleFor(t => t.Id, f => 0)
+                .RuleFor(t => t.MovieScreening, f => f.PickRandom(screenings))
                 .RuleFor(t => t.SeatNumber, f => f.Random.Number(1, 50))
                 .Generate(100);
 
